c623c81aeb4413ebed8efcf2dd1d9e4a12d977c6
Niyakit <niyakit123@gmail.com>
2023-02-08 17:16:41 +0700

    Add animation canvas component

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,48 @@
 use yew::prelude::*;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
+struct AnimationCanvas {
+    canvas: NodeRef
+}
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+pub enum AnimationCanvasMsg {
+
+}
+
+impl Component for AnimationCanvas {
+    type Message = AnimationCanvasMsg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            canvas: NodeRef::default(),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        true
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <canvas
+                    id="canvas"
+                    ref={self.canvas.clone()}>
+                </canvas>
+            </div>
+        }
     }
 }
+
+#[function_component(App)]
+fn app_body() -> Html {
+    html! {
+        <>
+            <AnimationCanvas/>
+        </>
+    }
+}
+
 
 fn main() {
     yew::Renderer::<App>::new().render();
